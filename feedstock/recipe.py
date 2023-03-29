6abfd96aa17d255a32f91698199a8c7a569a374e
jbusecke <julius@ldeo.columbia.edu>
2023-03-29 17:18:50 -0400

    refactor to injection PR on pgf runner

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -1,6 +1,6 @@
 ### needs to be here otherwise the import fails
 """Modified transforms from Pangeo Forge"""
-import json
+
 import apache_beam as beam
 from dataclasses import dataclass, field
 from typing import List, Dict, Union
@@ -9,62 +9,15 @@ from pangeo_forge_recipes.storage import FSSpecTarget
 from pangeo_forge_recipes.transforms import DetermineSchema, XarraySchema, IndexItems, PrepareZarrTarget, StoreDatasetFragments
 from pangeo_forge_recipes.transforms import OpenURLWithFSSpec, OpenWithXarray
 
-#### hardcode the json here (this is stupid)
-recipe_input_dict = {'CMIP6.CMIP.MOHC.UKESM1-0-LL.historical.r19i1p1f2.Omon.zmeso.gn.v20200430': {'urls': ['https://esgf-data1.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/MOHC/UKESM1-0-LL/historical/r19i1p1f2/Omon/zmeso/gn/v20200430/zmeso_Omon_UKESM1-0-LL_historical_r19i1p1f2_gn_185001-189912.nc',
-   'https://esgf-data1.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/MOHC/UKESM1-0-LL/historical/r19i1p1f2/Omon/zmeso/gn/v20200430/zmeso_Omon_UKESM1-0-LL_historical_r19i1p1f2_gn_190001-194912.nc',
-   'https://esgf-data1.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/MOHC/UKESM1-0-LL/historical/r19i1p1f2/Omon/zmeso/gn/v20200430/zmeso_Omon_UKESM1-0-LL_historical_r19i1p1f2_gn_195001-199912.nc'],
-  'instance_id': 'CMIP6.CMIP.MOHC.UKESM1-0-LL.historical.r19i1p1f2.Omon.zmeso.gn.v20200430',
-  'data_node': 'esgf-data1.llnl.gov'},
- 'CMIP6.CMIP.CCCma.CanESM5-CanOE.historical.r2i1p2f1.Omon.zmeso.gn.v20190429': {'urls': ['http://crd-esgf-drc.ec.gc.ca/thredds/fileServer/esgA_dataroot/AR6/CMIP6/CMIP/CCCma/CanESM5-CanOE/historical/r2i1p2f1/Omon/zmeso/gn/v20190429/zmeso_Omon_CanESM5-CanOE_historical_r2i1p2f1_gn_185001-186012.nc',
-   'http://crd-esgf-drc.ec.gc.ca/thredds/fileServer/esgA_dataroot/AR6/CMIP6/CMIP/CCCma/CanESM5-CanOE/historical/r2i1p2f1/Omon/zmeso/gn/v20190429/zmeso_Omon_CanESM5-CanOE_historical_r2i1p2f1_gn_186101-187012.nc',
-   'http://crd-esgf-drc.ec.gc.ca/thredds/fileServer/esgA_dataroot/AR6/CMIP6/CMIP/CCCma/CanESM5-CanOE/historical/r2i1p2f1/Omon/zmeso/gn/v20190429/zmeso_Omon_CanESM5-CanOE_historical_r2i1p2f1_gn_187101-188012.nc',
-   'http://crd-esgf-drc.ec.gc.ca/thredds/fileServer/esgA_dataroot/AR6/CMIP6/CMIP/CCCma/CanESM5-CanOE/historical/r2i1p2f1/Omon/zmeso/gn/v20190429/zmeso_Omon_CanESM5-CanOE_historical_r2i1p2f1_gn_188101-189012.nc',
-   'http://crd-esgf-drc.ec.gc.ca/thredds/fileServer/esgA_dataroot/AR6/CMIP6/CMIP/CCCma/CanESM5-CanOE/historical/r2i1p2f1/Omon/zmeso/gn/v20190429/zmeso_Omon_CanESM5-CanOE_historical_r2i1p2f1_gn_189101-190012.nc',
-   'http://crd-esgf-drc.ec.gc.ca/thredds/fileServer/esgA_dataroot/AR6/CMIP6/CMIP/CCCma/CanESM5-CanOE/historical/r2i1p2f1/Omon/zmeso/gn/v20190429/zmeso_Omon_CanESM5-CanOE_historical_r2i1p2f1_gn_190101-191012.nc',
-   'http://crd-esgf-drc.ec.gc.ca/thredds/fileServer/esgA_dataroot/AR6/CMIP6/CMIP/CCCma/CanESM5-CanOE/historical/r2i1p2f1/Omon/zmeso/gn/v20190429/zmeso_Omon_CanESM5-CanOE_historical_r2i1p2f1_gn_191101-192012.nc',
-   'http://crd-esgf-drc.ec.gc.ca/thredds/fileServer/esgA_dataroot/AR6/CMIP6/CMIP/CCCma/CanESM5-CanOE/historical/r2i1p2f1/Omon/zmeso/gn/v20190429/zmeso_Omon_CanESM5-CanOE_historical_r2i1p2f1_gn_192101-193012.nc',
-   'http://crd-esgf-drc.ec.gc.ca/thredds/fileServer/esgA_dataroot/AR6/CMIP6/CMIP/CCCma/CanESM5-CanOE/historical/r2i1p2f1/Omon/zmeso/gn/v20190429/zmeso_Omon_CanESM5-CanOE_historical_r2i1p2f1_gn_193101-194012.nc',
-   'http://crd-esgf-drc.ec.gc.ca/thredds/fileServer/esgA_dataroot/AR6/CMIP6/CMIP/CCCma/CanESM5-CanOE/historical/r2i1p2f1/Omon/zmeso/gn/v20190429/zmeso_Omon_CanESM5-CanOE_historical_r2i1p2f1_gn_194101-195012.nc',
-   'http://crd-esgf-drc.ec.gc.ca/thredds/fileServer/esgA_dataroot/AR6/CMIP6/CMIP/CCCma/CanESM5-CanOE/historical/r2i1p2f1/Omon/zmeso/gn/v20190429/zmeso_Omon_CanESM5-CanOE_historical_r2i1p2f1_gn_195101-196012.nc',
-   'http://crd-esgf-drc.ec.gc.ca/thredds/fileServer/esgA_dataroot/AR6/CMIP6/CMIP/CCCma/CanESM5-CanOE/historical/r2i1p2f1/Omon/zmeso/gn/v20190429/zmeso_Omon_CanESM5-CanOE_historical_r2i1p2f1_gn_196101-197012.nc',
-   'http://crd-esgf-drc.ec.gc.ca/thredds/fileServer/esgA_dataroot/AR6/CMIP6/CMIP/CCCma/CanESM5-CanOE/historical/r2i1p2f1/Omon/zmeso/gn/v20190429/zmeso_Omon_CanESM5-CanOE_historical_r2i1p2f1_gn_197101-198012.nc',
-   'http://crd-esgf-drc.ec.gc.ca/thredds/fileServer/esgA_dataroot/AR6/CMIP6/CMIP/CCCma/CanESM5-CanOE/historical/r2i1p2f1/Omon/zmeso/gn/v20190429/zmeso_Omon_CanESM5-CanOE_historical_r2i1p2f1_gn_198101-199012.nc',
-   'http://crd-esgf-drc.ec.gc.ca/thredds/fileServer/esgA_dataroot/AR6/CMIP6/CMIP/CCCma/CanESM5-CanOE/historical/r2i1p2f1/Omon/zmeso/gn/v20190429/zmeso_Omon_CanESM5-CanOE_historical_r2i1p2f1_gn_199101-200012.nc',
-   'http://crd-esgf-drc.ec.gc.ca/thredds/fileServer/esgA_dataroot/AR6/CMIP6/CMIP/CCCma/CanESM5-CanOE/historical/r2i1p2f1/Omon/zmeso/gn/v20190429/zmeso_Omon_CanESM5-CanOE_historical_r2i1p2f1_gn_200101-201012.nc',
-   'http://crd-esgf-drc.ec.gc.ca/thredds/fileServer/esgA_dataroot/AR6/CMIP6/CMIP/CCCma/CanESM5-CanOE/historical/r2i1p2f1/Omon/zmeso/gn/v20190429/zmeso_Omon_CanESM5-CanOE_historical_r2i1p2f1_gn_201101-201412.nc'],
-  'instance_id': 'CMIP6.CMIP.CCCma.CanESM5-CanOE.historical.r2i1p2f1.Omon.zmeso.gn.v20190429',
-  'data_node': 'crd-esgf-drc.ec.gc.ca'},
- 'CMIP6.CMIP.MOHC.UKESM1-0-LL.historical.r4i1p1f2.Omon.zmeso.gn.v20200204': {'urls': ['https://esgf.ceda.ac.uk/thredds/fileServer/esg_cmip6/CMIP6/CMIP/MOHC/UKESM1-0-LL/historical/r4i1p1f2/Omon/zmeso/gn/v20200204/zmeso_Omon_UKESM1-0-LL_historical_r4i1p1f2_gn_185001-189912.nc',
-   'https://esgf.ceda.ac.uk/thredds/fileServer/esg_cmip6/CMIP6/CMIP/MOHC/UKESM1-0-LL/historical/r4i1p1f2/Omon/zmeso/gn/v20200204/zmeso_Omon_UKESM1-0-LL_historical_r4i1p1f2_gn_190001-194912.nc',
-   'https://esgf.ceda.ac.uk/thredds/fileServer/esg_cmip6/CMIP6/CMIP/MOHC/UKESM1-0-LL/historical/r4i1p1f2/Omon/zmeso/gn/v20200204/zmeso_Omon_UKESM1-0-LL_historical_r4i1p1f2_gn_195001-199912.nc',
-   'https://esgf.ceda.ac.uk/thredds/fileServer/esg_cmip6/CMIP6/CMIP/MOHC/UKESM1-0-LL/historical/r4i1p1f2/Omon/zmeso/gn/v20200204/zmeso_Omon_UKESM1-0-LL_historical_r4i1p1f2_gn_200001-201412.nc'],
-  'instance_id': 'CMIP6.CMIP.MOHC.UKESM1-0-LL.historical.r4i1p1f2.Omon.zmeso.gn.v20200204',
-  'data_node': 'esgf.ceda.ac.uk'},
- 'CMIP6.CMIP.IPSL.IPSL-CM6A-LR.historical.r1i1p1f1.Omon.zmeso.gn.v20180803': {'urls': ['http://aims3.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/IPSL/IPSL-CM6A-LR/historical/r1i1p1f1/Omon/zmeso/gn/v20180803/zmeso_Omon_IPSL-CM6A-LR_historical_r1i1p1f1_gn_185001-194912.nc',
-   'http://aims3.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/IPSL/IPSL-CM6A-LR/historical/r1i1p1f1/Omon/zmeso/gn/v20180803/zmeso_Omon_IPSL-CM6A-LR_historical_r1i1p1f1_gn_195001-201412.nc'],
-  'instance_id': 'CMIP6.CMIP.IPSL.IPSL-CM6A-LR.historical.r1i1p1f1.Omon.zmeso.gn.v20180803',
-  'data_node': 'aims3.llnl.gov'},
- 'CMIP6.CMIP.IPSL.IPSL-CM6A-LR.historical.r14i1p1f1.Omon.zmeso.gn.v20180803': {'urls': ['http://aims3.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/IPSL/IPSL-CM6A-LR/historical/r14i1p1f1/Omon/zmeso/gn/v20180803/zmeso_Omon_IPSL-CM6A-LR_historical_r14i1p1f1_gn_185001-194912.nc',
-   'http://aims3.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/IPSL/IPSL-CM6A-LR/historical/r14i1p1f1/Omon/zmeso/gn/v20180803/zmeso_Omon_IPSL-CM6A-LR_historical_r14i1p1f1_gn_195001-201412.nc'],
-  'instance_id': 'CMIP6.CMIP.IPSL.IPSL-CM6A-LR.historical.r14i1p1f1.Omon.zmeso.gn.v20180803',
-  'data_node': 'aims3.llnl.gov'},
- 'CMIP6.CMIP.IPSL.IPSL-CM5A2-INCA.historical.r1i1p1f1.Omon.zmeso.gn.v20200729': {'urls': ['https://esgf-data1.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/IPSL/IPSL-CM5A2-INCA/historical/r1i1p1f1/Omon/zmeso/gn/v20200729/zmeso_Omon_IPSL-CM5A2-INCA_historical_r1i1p1f1_gn_185001-201412.nc'],
-  'instance_id': 'CMIP6.CMIP.IPSL.IPSL-CM5A2-INCA.historical.r1i1p1f1.Omon.zmeso.gn.v20200729',
-  'data_node': 'esgf-data1.llnl.gov'},
- 'CMIP6.CMIP.IPSL.IPSL-CM6A-LR.historical.r27i1p1f1.Omon.zmeso.gn.v20191204': {'urls': ['http://esgf-data1.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/IPSL/IPSL-CM6A-LR/historical/r27i1p1f1/Omon/zmeso/gn/v20191204/zmeso_Omon_IPSL-CM6A-LR_historical_r27i1p1f1_gn_185001-194912.nc',
-   'http://esgf-data1.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/IPSL/IPSL-CM6A-LR/historical/r27i1p1f1/Omon/zmeso/gn/v20191204/zmeso_Omon_IPSL-CM6A-LR_historical_r27i1p1f1_gn_195001-201412.nc'],
-  'instance_id': 'CMIP6.CMIP.IPSL.IPSL-CM6A-LR.historical.r27i1p1f1.Omon.zmeso.gn.v20191204',
-  'data_node': 'esgf-data1.llnl.gov'},
- 'CMIP6.CMIP.IPSL.IPSL-CM6A-LR.historical.r29i1p1f1.Omon.zmeso.gn.v20191204': {'urls': ['http://vesg.ipsl.upmc.fr/thredds/fileServer/cmip6/CMIP/IPSL/IPSL-CM6A-LR/historical/r29i1p1f1/Omon/zmeso/gn/v20191204/zmeso_Omon_IPSL-CM6A-LR_historical_r29i1p1f1_gn_185001-194912.nc',
-   'http://vesg.ipsl.upmc.fr/thredds/fileServer/cmip6/CMIP/IPSL/IPSL-CM6A-LR/historical/r29i1p1f1/Omon/zmeso/gn/v20191204/zmeso_Omon_IPSL-CM6A-LR_historical_r29i1p1f1_gn_195001-201412.nc'],
-  'instance_id': 'CMIP6.CMIP.IPSL.IPSL-CM6A-LR.historical.r29i1p1f1.Omon.zmeso.gn.v20191204',
-  'data_node': 'vesg.ipsl.upmc.fr'}}
-######
-
-# # load recipe input dictionary from json file
-
-# with open('recipe_input_dict.json', 'r') as f:
-#     recipe_input_dict = json.load(f)
+def urls_from_gcs(iid:str=None) -> List[str]:
+    """Get urls from GCS bucket"""
+    import gcsfs
+    import json
+    url_bucket = 'leap-persistent/jbusecke/cmip6urls'
+    fs = gcsfs.GCSFileSystem(project='leap-pangeo')
+    with fs.open(f"gs://{url_bucket}/{iid}.json", 'r') as f:
+        urls = json.load(f)['urls']
+    return urls
 
 def dynamic_target_chunks_from_schema(
     schema: XarraySchema, 
@@ -140,21 +93,17 @@ class StoreToZarr(beam.PTransform):
 
 # create recipe dictionary
 target_chunk_nbytes = int(100e6)
-recipes = {}
+input_urls = urls_from_gcs() # The iid input here gets ingected from pangeo-forge-runner (https://github.com/pangeo-forge/pangeo-forge-runner/pull/67)
 
-for iid, input_dict in recipe_input_dict.items():
-    input_urls = input_dict['urls']
-
-    pattern = pattern_from_file_sequence(input_urls, concat_dim='time')
-    transforms = (
-        beam.Create(pattern.items())
-        | OpenURLWithFSSpec()
-        | OpenWithXarray(xarray_open_kwargs={'use_cftime':True}) # do not specify file type to accomdate both ncdf3 and ncdf4
-        | StoreToZarr(
-            # store_name=f"{iid}.zarr",
-            combine_dims=pattern.combine_dim_keys,
-            target_chunk_nbytes=target_chunk_nbytes,
-            chunk_dim=pattern.concat_dims[0] # not sure if this is better than hardcoding?
-        )
+pattern = pattern_from_file_sequence(input_urls, concat_dim='time')
+transforms = (
+    beam.Create(pattern.items())
+    | OpenURLWithFSSpec()
+    | OpenWithXarray(xarray_open_kwargs={'use_cftime':True}) # do not specify file type to accomdate both ncdf3 and ncdf4
+    | StoreToZarr(
+        # store_name=f"{iid}.zarr",
+        combine_dims=pattern.combine_dim_keys,
+        target_chunk_nbytes=target_chunk_nbytes,
+        chunk_dim=pattern.concat_dims[0] # not sure if this is better than hardcoding?
     )
-    recipes[iid] = transforms
+)
