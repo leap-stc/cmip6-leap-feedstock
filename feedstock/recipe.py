8fb5f701a4bb0779d9f81ae1574ebd1c918d03b5
jbusecke <julius@ldeo.columbia.edu>
2023-07-29 14:40:29 +0200

    Debugging local pgf-runner with hardcoded iids

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -10,20 +10,43 @@ from pangeo_forge_recipes.transforms import (
 
 def get_iid(iid: str=None) -> str:
     """pangeo-forge-runner injection func"""
-    return iid
+    # return iid
+    return 'CMIP6.CMIP.MRI.MRI-ESM2-0.historical.r3i1p1f1.day.pr.gn.v20190603'
 
 iid = get_iid() # The iid input here gets ingected from pangeo-forge-runner (https://github.com/pangeo-forge/pangeo-forge-runner/pull/67)
 
-def urls_from_gcs(iid: str) -> List[str]:
-    """Get urls from GCS bucket"""
-    import gcsfs
-    import json
-    print(f"Fetching urls from GCS for {iid}")
-    url_bucket = 'leap-persistent/jbusecke/cmip6urls'
-    fs = gcsfs.GCSFileSystem(project='leap-pangeo')
-    with fs.open(f"gs://{url_bucket}/{iid}.json", 'r') as f:
-        urls = json.load(f)['urls']
-    return urls
+# def urls_from_gcs(iid: str) -> List[str]:
+#     """Get urls from GCS bucket"""
+#     import gcsfs
+#     import json
+#     print(f"Fetching urls from GCS for {iid}")
+#     url_bucket = 'leap-persistent/jbusecke/cmip6urls'
+#     fs = gcsfs.GCSFileSystem(project='leap-pangeo')
+#     with fs.open(f"gs://{url_bucket}/{iid}.json", 'r') as f:
+#         urls = json.load(f)['urls']
+#     return urls
+
+# new version that uses the BQ database
+def urls_from_bq(iid: str) -> List[str]:
+    # """Get URLS from bigquery"""
+    # from bigquery import BigQueryInterface # this naming is dicey...
+    # from google.cloud import bigquery
+
+    # table_id = 'leap-pangeo.testcmip6.stores_v2'
+    # client = bigquery.Client()
+    # bq_interface = BigQueryInterface(client, table_id)
+    # iid_obj = bq_interface.get_iid_results(iid)
+    # print(f"{iid_obj.exists=}")
+    # print(iid_obj.results)
+
+    # return list(iid_obj.results)[0][2] #FIXME: This is pretty ugly...just a quick test for now.
+    
+    # !!!Bypass for now and hardcode the urls
+    return ['http://aims3.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/MRI/MRI-ESM2-0/historical/r3i1p1f1/day/pr/gn/v20190603/pr_day_MRI-ESM2-0_historical_r3i1p1f1_gn_18500101-18991231.nc',
+ 'http://aims3.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/MRI/MRI-ESM2-0/historical/r3i1p1f1/day/pr/gn/v20190603/pr_day_MRI-ESM2-0_historical_r3i1p1f1_gn_19000101-19491231.nc',
+ 'http://aims3.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/MRI/MRI-ESM2-0/historical/r3i1p1f1/day/pr/gn/v20190603/pr_day_MRI-ESM2-0_historical_r3i1p1f1_gn_19500101-19991231.nc',
+ 'http://aims3.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/MRI/MRI-ESM2-0/historical/r3i1p1f1/day/pr/gn/v20190603/pr_day_MRI-ESM2-0_historical_r3i1p1f1_gn_20000101-20141231.nc']
+
 
 # just to be sure that only the actual variable_id is used as a dataset variable
 class KeepOnlyVariableId(beam.PTransform):
@@ -46,19 +69,19 @@ class KeepOnlyVariableId(beam.PTransform):
         return pcoll | beam.Map(self._keep_only_variable_id)
 
 # create recipe dictionary
-target_chunk_nbytes = int(100e6)
-input_urls = urls_from_gcs(iid)
+input_urls = urls_from_bq(iid)
 print(f'Creating recipe from {input_urls}')
 pattern = pattern_from_file_sequence(input_urls, concat_dim='time')
 transforms = (
     beam.Create(pattern.items())
     | OpenURLWithFSSpec()
     | OpenWithXarray(xarray_open_kwargs={"use_cftime":True}) # do not specify file type to accomdate both ncdf3 and ncdf4
-    | KeepOnlyVariableId() #still necessary ('CMIP6.CMIP.IPSL.IPSL-CM5A2-INCA.historical.r1i1p1f1.Omon.zmeso.gn.v20200729' is an example)
+    | KeepOnlyVariableId()
     | StoreToZarr(
         store_name=f"{iid}.zarr",
         combine_dims=pattern.combine_dim_keys,
-        target_chunk_nbytes=target_chunk_nbytes,
-        chunk_dim='time'
+        target_chunk_size='200MB',
+        target_chunks_aspect_ratio = {'x': 1, 'y': 1, 'time': 10}, # TODO: this will fail for many of the datasets with different naming. 
+        # Need to improve this upstream (but how do I generalize this?)
     )
 )
