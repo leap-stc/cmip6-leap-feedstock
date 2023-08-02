67e64ec10e46ceb3314d53ff63e44f9bf2d9391b
jbusecke <julius@ldeo.columbia.edu>
2023-08-02 19:01:59 +0200

    instead revert to main for now

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -70,107 +70,107 @@ class KeepOnlyVariableId(beam.PTransform):
     def expand(self, pcoll: beam.PCollection) -> beam.PCollection:
         return pcoll | beam.Map(self._keep_only_variable_id)
 
-# debug the recipe locally
-from pangeo_forge_recipes.aggregation import XarraySchema
-from pangeo_forge_recipes.dynamic_target_chunks import dynamic_target_chunks_from_schema
-from pangeo_forge_recipes.patterns import Dimension
-from pangeo_forge_recipes.writers import ZarrWriterMixin
-from pangeo_forge_recipes.transforms import DetermineSchema, IndexItems, Rechunk, PrepareZarrTarget, StoreDatasetFragments
-from dataclasses import dataclass, field
+# # debug the recipe locally
+# from pangeo_forge_recipes.aggregation import XarraySchema
+# from pangeo_forge_recipes.dynamic_target_chunks import dynamic_target_chunks_from_schema
+# from pangeo_forge_recipes.patterns import Dimension
+# from pangeo_forge_recipes.writers import ZarrWriterMixin
+# from pangeo_forge_recipes.transforms import DetermineSchema, IndexItems, Rechunk, PrepareZarrTarget, StoreDatasetFragments
+# from dataclasses import dataclass, field
 
-from typing import Dict, Optional, Union, cast
+# from typing import Dict, Optional, Union, cast
 
-@dataclass
-class StoreToZarr(beam.PTransform, ZarrWriterMixin):
-    """Store a PCollection of Xarray datasets to Zarr.
+# @dataclass
+# class StoreToZarr(beam.PTransform, ZarrWriterMixin):
+#     """Store a PCollection of Xarray datasets to Zarr.
 
-    :param combine_dims: The dimensions to combine
-    :param target_chunks: Dictionary mapping dimension names to chunks sizes.
-        If a dimension is a not named, the chunks will be inferred from the data.
-    :param target_chunk_aspect_ratio: Dictionary mapping dimension names to desired
-        aspect ratio of total number of chunks along each dimension.
-    :param target_chunk_size: Desired single chunks size. Can be provided as
-        integer (bytes) or as a str like '100MB' etc.
-    :param size_tolerance : float, optional
-        Chunksize tolerance. Resulting chunk size will be within
-        [target_chunk_size*(1-size_tolerance),
-        target_chunk_size*(1+size_tolerance)] , by default 0.2
-    """
+#     :param combine_dims: The dimensions to combine
+#     :param target_chunks: Dictionary mapping dimension names to chunks sizes.
+#         If a dimension is a not named, the chunks will be inferred from the data.
+#     :param target_chunk_aspect_ratio: Dictionary mapping dimension names to desired
+#         aspect ratio of total number of chunks along each dimension.
+#     :param target_chunk_size: Desired single chunks size. Can be provided as
+#         integer (bytes) or as a str like '100MB' etc.
+#     :param size_tolerance : float, optional
+#         Chunksize tolerance. Resulting chunk size will be within
+#         [target_chunk_size*(1-size_tolerance),
+#         target_chunk_size*(1+size_tolerance)] , by default 0.2
+#     """
 
-    # TODO: make it so we don't have to explicitly specify combine_dims
-    # Could be inferred from the pattern instead
-    combine_dims: List[Dimension]
-    target_chunks: Optional[Dict[str, int]] = field(default=None)
-    target_chunks_aspect_ratio: Optional[Dict[str, int]] = field(default=None)
-    target_chunk_size: Optional[Union[str, int]] = field(
-        default=None
-    )  # ? Should we provide a default?
-    size_tolerance: float = 0.2
+#     # TODO: make it so we don't have to explicitly specify combine_dims
+#     # Could be inferred from the pattern instead
+#     combine_dims: List[Dimension]
+#     target_chunks: Optional[Dict[str, int]] = field(default=None)
+#     target_chunks_aspect_ratio: Optional[Dict[str, int]] = field(default=None)
+#     target_chunk_size: Optional[Union[str, int]] = field(
+#         default=None
+#     )  # ? Should we provide a default?
+#     size_tolerance: float = 0.2
 
-    def __post_init__(self):
-        # if none of the chunking parameters is specified, set the default behavior
-        # of target_chunks={}
-        if all(
-            a is None
-            for a in (self.target_chunks, self.target_chunk_size, self.target_chunks_aspect_ratio)
-        ):
-            self.target_chunks = {}
+#     def __post_init__(self):
+#         # if none of the chunking parameters is specified, set the default behavior
+#         # of target_chunks={}
+#         if all(
+#             a is None
+#             for a in (self.target_chunks, self.target_chunk_size, self.target_chunks_aspect_ratio)
+#         ):
+#             self.target_chunks = {}
 
-        # check that not both static and dynamic chunking are specified
-        if self.target_chunks is not None and (
-            self.target_chunk_size is not None or self.target_chunks_aspect_ratio is not None
-        ):
-            raise ValueError(
-                (
-                    "Cannot specify both target_chunks and "
-                    "target_chunk_size or target_chunks_aspect_ratio."
-                )
-            )
+#         # check that not both static and dynamic chunking are specified
+#         if self.target_chunks is not None and (
+#             self.target_chunk_size is not None or self.target_chunks_aspect_ratio is not None
+#         ):
+#             raise ValueError(
+#                 (
+#                     "Cannot specify both target_chunks and "
+#                     "target_chunk_size or target_chunks_aspect_ratio."
+#                 )
+#             )
 
-        # if dynamic chunking is specified, make sure both target_chunk_size
-        # and target_chunks_aspect_ratio are specified
-        if self.target_chunks is None and not (
-            self.target_chunk_size is not None and self.target_chunks_aspect_ratio is not None
-        ):
-            raise ValueError(
-                (
-                    "Must specify both target_chunk_size and "
-                    "target_chunks_aspect_ratio to enable dynamic chunking."
-                )
-            )
+#         # if dynamic chunking is specified, make sure both target_chunk_size
+#         # and target_chunks_aspect_ratio are specified
+#         if self.target_chunks is None and not (
+#             self.target_chunk_size is not None and self.target_chunks_aspect_ratio is not None
+#         ):
+#             raise ValueError(
+#                 (
+#                     "Must specify both target_chunk_size and "
+#                     "target_chunks_aspect_ratio to enable dynamic chunking."
+#                 )
+#             )
 
-    def determine_target_chunks(self, schema: XarraySchema) -> Dict[str, int]:
-        # if dynamic chunking is chosen, set the objects target_chunks here
-        # We need to explicitly cast the types here because we know
-        # that for the combinations chosen here (dynamic vs static chunking)
-        # the type of the input is never None (our __post_init__ method takes
-        # care of raising errors if this is the case).
-        if self.target_chunks_aspect_ratio is not None:
-            target_chunks = dynamic_target_chunks_from_schema(
-                schema,
-                target_chunks = {'time':10},
-                # target_chunk_size=cast(Union[int, str], self.target_chunk_size),
-                # target_chunks_aspect_ratio=cast(Dict[str, int], self.target_chunks_aspect_ratio),
-                # size_tolerance=self.size_tolerance,
-            )
-        else:
-            target_chunks = cast(Dict[str, int], self.target_chunks)
-        return target_chunks
+#     def determine_target_chunks(self, schema: XarraySchema) -> Dict[str, int]:
+#         # if dynamic chunking is chosen, set the objects target_chunks here
+#         # We need to explicitly cast the types here because we know
+#         # that for the combinations chosen here (dynamic vs static chunking)
+#         # the type of the input is never None (our __post_init__ method takes
+#         # care of raising errors if this is the case).
+#         if self.target_chunks_aspect_ratio is not None:
+#             target_chunks = dynamic_target_chunks_from_schema(
+#                 schema,
+#                 target_chunks = {'time':10},
+#                 # target_chunk_size=cast(Union[int, str], self.target_chunk_size),
+#                 # target_chunks_aspect_ratio=cast(Dict[str, int], self.target_chunks_aspect_ratio),
+#                 # size_tolerance=self.size_tolerance,
+#             )
+#         else:
+#             target_chunks = cast(Dict[str, int], self.target_chunks)
+#         return target_chunks
 
-    def expand(self, datasets: beam.PCollection) -> beam.PCollection:
-        schema = datasets | DetermineSchema(combine_dims=self.combine_dims)
+#     def expand(self, datasets: beam.PCollection) -> beam.PCollection:
+#         schema = datasets | DetermineSchema(combine_dims=self.combine_dims)
 
-        target_chunks = beam.pvalue.AsSingleton(
-            schema | beam.Map(
-                self.determine_target_chunks
-            )  # Would beam.pvalue.AsSingleton(self.determine_target_chunks(schema)) work?
-        )
-        indexed_datasets = datasets | IndexItems(schema=schema)
-        rechunked_datasets = indexed_datasets | Rechunk(target_chunks=target_chunks, schema=schema)
-        target_store = schema | PrepareZarrTarget(
-            target=self.get_full_target(), target_chunks=target_chunks
-        )
-        return rechunked_datasets | StoreDatasetFragments(target_store=target_store)
+#         target_chunks = beam.pvalue.AsSingleton(
+#             schema | beam.Map(
+#                 self.determine_target_chunks
+#             )  # Would beam.pvalue.AsSingleton(self.determine_target_chunks(schema)) work?
+#         )
+#         indexed_datasets = datasets | IndexItems(schema=schema)
+#         rechunked_datasets = indexed_datasets | Rechunk(target_chunks=target_chunks, schema=schema)
+#         target_store = schema | PrepareZarrTarget(
+#             target=self.get_full_target(), target_chunks=target_chunks
+#         )
+#         return rechunked_datasets | StoreDatasetFragments(target_store=target_store)
 
 
 
