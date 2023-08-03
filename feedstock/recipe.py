8cc334d8f3b92e98dd09c5d5e4527280b80a2c92
jbusecke <julius@ldeo.columbia.edu>
2023-08-03 13:05:15 +0200

    do not pass bounds as static chunks

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -87,7 +87,7 @@ TEST_CMIP = (
         # for now try with static chunking
         # target_chunks={'time': 1000}
         # Try to use the dynamically inferred chunking statically (see https://github.com/leap-stc/cmip6-leap-feedstock/pull/4#issuecomment-1663137723)
-        target_chunks={'time':3545, 'lat':80, 'lon':160, 'bnds':1},
+        target_chunks={'time':3545, 'lat':80, 'lon':160},
         # target_chunk_size='200MB',
         # target_chunks_aspect_ratio = {'lon': 1, 'lat': 1, 'time': 10, 'bnds':1}, # TODO: this will fail for many of the datasets with different naming. 
         # Need to improve this upstream (but how do I generalize this?)
