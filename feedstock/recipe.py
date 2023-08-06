5ebd70555ac0613da67da98552aec97961408a12
jbusecke <julius@ldeo.columbia.edu>
2023-08-06 18:23:59 +0200

    Try two iids

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -8,13 +8,19 @@ from pangeo_forge_recipes.transforms import (
     OpenURLWithFSSpec, OpenWithXarray, StoreToZarr, Indexed, T
 )
 
+iid_list = [
+    'CMIP6.CMIP.MRI.MRI-ESM2-0.historical.r3i1p1f1.day.pr.gn.v20190603'
+    'CMIP6.CMIP.NOAA-GFDL.GFDL-CM4.historical.r1i1p1f1.Amon.tas.gr1.v20180701'
+    ]
+
+
 
 
 def get_iid(iid: str=None) -> str:
     """pangeo-forge-runner injection func"""
     # return iid
     # return 'CMIP6.CMIP.MRI.MRI-ESM2-0.historical.r3i1p1f1.day.pr.gn.v20190603'
-    return 'CMIP6.CMIP.NOAA-GFDL.GFDL-CM4.historical.r1i1p1f1.Amon.tas.gr1.v20180701'
+    return 
 
 iid = get_iid() # The iid input here gets ingected from pangeo-forge-runner (https://github.com/pangeo-forge/pangeo-forge-runner/pull/67)
 
@@ -45,12 +51,20 @@ def urls_from_bq(iid: str) -> List[str]:
     # return list(iid_obj.results)[0][2] #FIXME: This is pretty ugly...just a quick test for now.
     
     # !!!Bypass for now and hardcode the urls
-#     return ['http://aims3.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/MRI/MRI-ESM2-0/historical/r3i1p1f1/day/pr/gn/v20190603/pr_day_MRI-ESM2-0_historical_r3i1p1f1_gn_18500101-18991231.nc',
-#  'http://aims3.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/MRI/MRI-ESM2-0/historical/r3i1p1f1/day/pr/gn/v20190603/pr_day_MRI-ESM2-0_historical_r3i1p1f1_gn_19000101-19491231.nc',
-#  'http://aims3.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/MRI/MRI-ESM2-0/historical/r3i1p1f1/day/pr/gn/v20190603/pr_day_MRI-ESM2-0_historical_r3i1p1f1_gn_19500101-19991231.nc',
-#  'http://aims3.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/MRI/MRI-ESM2-0/historical/r3i1p1f1/day/pr/gn/v20190603/pr_day_MRI-ESM2-0_historical_r3i1p1f1_gn_20000101-20141231.nc']
-    return ['http://esgf-data04.diasjp.net/thredds/fileServer/esg_dataroot/CMIP6/CMIP/NOAA-GFDL/GFDL-CM4/historical/r1i1p1f1/Amon/tas/gr1/v20180701/tas_Amon_GFDL-CM4_historical_r1i1p1f1_gr1_185001-194912.nc',
- 'http://esgf-data04.diasjp.net/thredds/fileServer/esg_dataroot/CMIP6/CMIP/NOAA-GFDL/GFDL-CM4/historical/r1i1p1f1/Amon/tas/gr1/v20180701/tas_Amon_GFDL-CM4_historical_r1i1p1f1_gr1_195001-201412.nc']
+    url_dict = {
+        'CMIP6.CMIP.MRI.MRI-ESM2-0.historical.r3i1p1f1.day.pr.gn.v20190603':[
+            'http://aims3.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/MRI/MRI-ESM2-0/historical/r3i1p1f1/day/pr/gn/v20190603/pr_day_MRI-ESM2-0_historical_r3i1p1f1_gn_18500101-18991231.nc',
+            'http://aims3.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/MRI/MRI-ESM2-0/historical/r3i1p1f1/day/pr/gn/v20190603/pr_day_MRI-ESM2-0_historical_r3i1p1f1_gn_19000101-19491231.nc',
+            'http://aims3.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/MRI/MRI-ESM2-0/historical/r3i1p1f1/day/pr/gn/v20190603/pr_day_MRI-ESM2-0_historical_r3i1p1f1_gn_19500101-19991231.nc',
+            'http://aims3.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/MRI/MRI-ESM2-0/historical/r3i1p1f1/day/pr/gn/v20190603/pr_day_MRI-ESM2-0_historical_r3i1p1f1_gn_20000101-20141231.nc'
+        ],
+        'CMIP6.CMIP.NOAA-GFDL.GFDL-CM4.historical.r1i1p1f1.Amon.tas.gr1.v20180701': [
+            'http://esgf-data04.diasjp.net/thredds/fileServer/esg_dataroot/CMIP6/CMIP/NOAA-GFDL/GFDL-CM4/historical/r1i1p1f1/Amon/tas/gr1/v20180701/tas_Amon_GFDL-CM4_historical_r1i1p1f1_gr1_185001-194912.nc',
+            'http://esgf-data04.diasjp.net/thredds/fileServer/esg_dataroot/CMIP6/CMIP/NOAA-GFDL/GFDL-CM4/historical/r1i1p1f1/Amon/tas/gr1/v20180701/tas_Amon_GFDL-CM4_historical_r1i1p1f1_gr1_195001-201412.nc'
+        ],
+    }
+
+    return url_dict[iid]
 
 # just to be sure that only the actual variable_id is used as a dataset variable
 class KeepOnlyVariableId(beam.PTransform):
@@ -75,23 +89,23 @@ class KeepOnlyVariableId(beam.PTransform):
 
 
 # create recipe dictionary
-input_urls = urls_from_bq(iid)
-print(f'Creating recipe from {input_urls}')
-pattern = pattern_from_file_sequence(input_urls, concat_dim='time')
-TEST_CMIP = (
-    beam.Create(pattern.items())
-    | OpenURLWithFSSpec()
-    | OpenWithXarray(xarray_open_kwargs={"use_cftime":True}) # do not specify file type to accomdate both ncdf3 and ncdf4
-    | KeepOnlyVariableId()
-    | StoreToZarr(
-        store_name=f"{iid}.zarr",
-        combine_dims=pattern.combine_dim_keys,
-        # for now try with static chunking
-        # target_chunks={'time': 1000}
-        # Try to use the dynamically inferred chunking statically (see https://github.com/leap-stc/cmip6-leap-feedstock/pull/4#issuecomment-1663137723)
-        # target_chunks={'time':3545, 'lat':80, 'lon':160},
-        target_chunk_size='200MB',
-        target_chunks_aspect_ratio = {'lon': -1, 'lat': -1, 'time': 1, 'bnds':-1}, # TODO: this will fail for many of the datasets with different naming. 
-        # Need to improve this upstream (but how do I generalize this?)
+
+target_chunks_aspect_ratio = {'lon': -1, 'lat': -1, 'time': 1} # all bounds should remain unchunked
+
+transforms = {}
+for iid in iid_list:
+    input_urls = urls_from_bq(iid)
+    print(f'Creating recipe from {input_urls}')
+    pattern = pattern_from_file_sequence(input_urls, concat_dim='time')
+    transforms[iid] = (
+        beam.Create(pattern.items())
+        | OpenURLWithFSSpec()
+        | OpenWithXarray(xarray_open_kwargs={"use_cftime":True}) # do not specify file type to accomdate both ncdf3 and ncdf4
+        | KeepOnlyVariableId()
+        | StoreToZarr(
+            store_name=f"{iid}.zarr",
+            combine_dims=pattern.combine_dim_keys,
+            target_chunk_size='200MB',
+            target_chunks_aspect_ratio = target_chunks_aspect_ratio,
+        )
     )
-)
