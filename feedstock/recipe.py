cfe78e2a6078fcf3a087bd00d3108712e1b5c87d
jbusecke <julius@ldeo.columbia.edu>
2023-08-08 00:41:34 +0200

    increase tolerance

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -56,6 +56,6 @@ for iid, urls in url_dict.items():
             combine_dims=pattern.combine_dim_keys,
             target_chunk_size='150MB',
             target_chunks_aspect_ratio = target_chunks_aspect_ratio,
-            size_tolerance=0.3
+            size_tolerance=0.5
             )
         )
