e232751c907a8e3389a4833e1842ba63d6a432a6
jbusecke <julius@ldeo.columbia.edu>
2023-08-23 13:55:31 -0400

    Add bigquery interface + some cleanup

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -1,17 +1,18 @@
 ### needs to be here otherwise the import fails
 """Modified transforms from Pangeo Forge"""
-
+from bigquery_interface import BQInterface, IIDEntry
 import apache_beam as beam
 from dataclasses import dataclass
 from datetime import datetime
 from typing import List
 import json
-from pangeo_forge_recipes.patterns import pattern_from_file_sequence
+from pangeo_forge_recipes.patterns import pattern_from_file_sequence, FilePattern
 from pangeo_forge_recipes.transforms import (
     OpenURLWithFSSpec, OpenWithXarray, StoreToZarr, Indexed, T
 )
 import xarray as xr
 import zarr
+from bigquery_interface import BQInterface, IIDEntry
 
 @dataclass
 class Preprocessor(beam.PTransform):
@@ -103,13 +104,28 @@ class TestDataset(beam.PTransform):
 # will start a new PR to try to refactor this as a dict-object, after 
 # checking in with Charles (https://github.com/leap-stc/cmip6-leap-feedstock/pull/4#issuecomment-1666929555)
 
-with open('feedstock/tim_batch.json') as json_file:
-# with open('feedstock/first_batch.json') as json_file:
+with open('feedstock/first_batch.json') as json_file:
+# with open('feedstock/tim_batch.json') as json_file:
+# with open('feedstock/second_batch.json') as json_file:
     url_dict = json.load(json_file)
 
-target_chunks_aspect_ratio = {'time': 1}
+# Prune the url dict to only include items that have not been logged to BQ yet
+print("Pruning url_dict")
+table_id = 'leap-pangeo.testcmip6.cmip6_feedstock_test2'
+bq_interface = BQInterface(table_id=table_id)
+url_dict_pruned = {}
+for iid, urls in url_dict.items():
+    if not bq_interface.iid_exists(iid):
+        url_dict_pruned[iid] = urls
+    else:
+        print(f"{iid =} already exists in {table_id =}")
+print(f"Pruned {len(url_dict) - len(url_dict_pruned)} items from url_dict")
 
+
+## Create the recipes
+target_chunks_aspect_ratio = {'time': 1}
 recipes = {}
+
 for iid, urls in url_dict.items():
     pattern = pattern_from_file_sequence(
         urls,
@@ -129,5 +145,6 @@ for iid, urls in url_dict.items():
             size_tolerance=0.5,
             allow_fallback_algo=True,
             )
-        # | TestDataset(iid=iid)
+        | TestDataset(iid=iid)
+        # | LogToBigQuery(iid=iid)
         )
