ce165e85a400da8fb5df442007ec943d55f2e0ba
Julius Busecke <julius@ldeo.columbia.edu>
2023-11-28 08:26:22 -0500

    Update recipe.py

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -204,11 +204,12 @@ overwrite_iids = [
     "CMIP6.ScenarioMIP.NCAR.CESM2-FV2.ssp585.r1i2p2f1.SImon.siitdthick.gn.v20220915",
     "CMIP6.CMIP.NCC.NorESM2-MM.historical.r1i1p1f1.SImon.sifb.gn.v20191108",
     "CMIP6.ScenarioMIP.NASA-GISS.GISS-E2-1-G-CC.ssp245.r102i1p1f1.Omon.chl.gn.v20220115",
-    "CMIP6.HighResMIP.MOHC.HadGEM3-GC31-HH.highres-future.r1i1p1f1.Omon.thetao.gn.v20200514",
-    "CMIP6.HighResMIP.NERC.HadGEM3-GC31-HH.hist-1950.r1i1p1f1.Omon.thetao.gn.v20200514",
-    "CMIP6.HighResMIP.MOHC.HadGEM3-GC31-HH.highres-future.r1i1p1f1.Omon.so.gn.v20200514",
-    "CMIP6.HighResMIP.NERC.HadGEM3-GC31-HH.hist-1950.r1i1p1f1.Omon.so.gn.v20200514",
+    # "CMIP6.HighResMIP.MOHC.HadGEM3-GC31-HH.highres-future.r1i1p1f1.Omon.thetao.gn.v20200514",
+    # "CMIP6.HighResMIP.NERC.HadGEM3-GC31-HH.hist-1950.r1i1p1f1.Omon.thetao.gn.v20200514",
+    # "CMIP6.HighResMIP.MOHC.HadGEM3-GC31-HH.highres-future.r1i1p1f1.Omon.so.gn.v20200514",
+    # "CMIP6.HighResMIP.NERC.HadGEM3-GC31-HH.hist-1950.r1i1p1f1.Omon.so.gn.v20200514",
 ]
+#deactivate high res runs until we find out what is going on there.
 
 # beam does NOT like to pickle client objects (https://github.com/googleapis/google-cloud-python/issues/3191#issuecomment-289151187)
 del bq_interface 
