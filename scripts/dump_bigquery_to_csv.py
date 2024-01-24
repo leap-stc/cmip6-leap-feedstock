3e13c6caabd2a7b1ebc555ae5a7ae34a364f0200
Julius Busecke <julius@ldeo.columbia.edu>
2024-01-23 19:00:06 -0500

    Run black and upload html report to bucket

--- scripts/dump_bigquery_to_csv.py
+++ scripts/dump_bigquery_to_csv.py
@@ -5,18 +5,18 @@ import os
 import gcsfs
 from datetime import date
 
-table_id = 'leap-pangeo.testcmip6.cmip6_consolidated_manual_testing'
+table_id = "leap-pangeo.testcmip6.cmip6_consolidated_manual_testing"
 target_prefix = "gs://cmip6/cmip6-pgf-ingestion-test/catalog/"
 
 bq = CMIPBQInterface(table_id=table_id)
 
 df_all = bq.get_latest()
 
-df_retracted = df_all[df_all['retracted'] == True]
-df_all_wo_retracted = df_all[df_all['retracted'] == False]
+df_retracted = df_all[df_all["retracted"] == True]
+df_all_wo_retracted = df_all[df_all["retracted"] == False]
 
-df_qc_failed = df_all_wo_retracted[df_all_wo_retracted['tests_passed'] == False]
-df_qc = df_all_wo_retracted[df_all_wo_retracted['tests_passed'] == True]
+df_qc_failed = df_all_wo_retracted[df_all_wo_retracted["tests_passed"] == False]
+df_qc = df_all_wo_retracted[df_all_wo_retracted["tests_passed"] == True]
 
 fs = gcsfs.GCSFileSystem()
 
@@ -25,17 +25,14 @@ for filename, bq_df in [
     ("noqc", df_qc_failed),
     ("retracted", df_retracted),
 ]:
-    path = os.path.join(
-        target_prefix, 
-        f"pangeo_esgf_zarr_{filename}.csv"
-    )
+    path = os.path.join(target_prefix, f"pangeo_esgf_zarr_{filename}.csv")
     path_backup = os.path.join(
-        target_prefix, 
-        'backups',
-        f"pangeo_esgf_zarr_{filename}_backup_{date.today()}.csv"
+        target_prefix,
+        "backups",
+        f"pangeo_esgf_zarr_{filename}_backup_{date.today()}.csv",
     )
-    
-    if len(bq_df)> 0:
+
+    if len(bq_df) > 0:
         intake_esm_df = bq_df_to_intake_esm(bq_df)
         intake_esm_df.to_csv(filename, index=False)
         if fs.exists(path):
--- scripts/retraction.py
+++ scripts/retraction.py
@@ -1,5 +1,7 @@
 import asyncio
+import datetime
 import httpx
+import gcsfs
 from tqdm.asyncio import tqdm
 from leap_data_management_utils.cmip_utils import IIDEntry, CMIPBQInterface
 
@@ -12,31 +14,32 @@ max_connections = 5
 semaphore = asyncio.Semaphore(max_connections)
 limits = httpx.Limits(max_connections=3, max_keepalive_connections=1)
 
-async def fetch_batch_instance_ids(client: httpx.AsyncClient, url:str, params: dict, offset:int, batchsize:int) -> list[str]:
-    async with semaphore: 
-        response = await client.get(url, params=params|{'offset':offset, 'limit':batchsize})
-        return [ d['instance_id'] for d in response.json()['response']['docs']]
+
+async def fetch_batch_instance_ids(
+    client: httpx.AsyncClient, url: str, params: dict, offset: int, batchsize: int
+) -> list[str]:
+    async with semaphore:
+        response = await client.get(
+            url, params=params | {"offset": offset, "limit": batchsize}
+        )
+        return [d["instance_id"] for d in response.json()["response"]["docs"]]
+
 
 async def fetch_instance_ids(url, params):
     async with httpx.AsyncClient(limits=limits, timeout=30.0) as client:
         # Initial response
-        init_response = await client.get(url, params=params|{'offset':0, 'limit':1})
-        n_res = init_response.json()['response']['numFound']
-        
+        init_response = await client.get(url, params=params | {"offset": 0, "limit": 1})
+        n_res = init_response.json()["response"]["numFound"]
+
         # fetch batches
         batchsize = 10000
         tasks = [
             asyncio.ensure_future(
-                fetch_batch_instance_ids(
-                    client,
-                    url,
-                    params,
-                    i,
-                    batchsize
-                )
-            ) for i in range(0,n_res, batchsize)
+                fetch_batch_instance_ids(client, url, params, i, batchsize)
+            )
+            for i in range(0, n_res, batchsize)
         ]
-        
+
         retracted = await tqdm.gather(*tasks)
 
         retracted_flat = []
@@ -44,14 +47,14 @@ async def fetch_instance_ids(url, params):
             retracted_flat.extend(l)
         return retracted_flat
 
-if __name__ ==  '__main__':
 
-    table_id = 'leap-pangeo.testcmip6.cmip6_consolidated_manual_testing' #TODO: change to production table
+if __name__ == "__main__":
+    table_id = "leap-pangeo.testcmip6.cmip6_consolidated_manual_testing"  # TODO: change to production table
     bq = CMIPBQInterface(table_id)
     params = {
         "type": "Dataset",
         "mip_era": "CMIP6",
-        "replica": "none", # this is horribly documented! But `none` should give both replicas and originals.
+        "replica": "none",  # this is horribly documented! But `none` should give both replicas and originals.
         "distrib": "true",
         "retracted": "true",
         "format": "application/solr+json",
@@ -60,33 +63,50 @@ if __name__ ==  '__main__':
     url = "http://esgf-node.llnl.gov/esg-search/search"
 
     loop = asyncio.get_event_loop()
-    retracted_iids = loop.run_until_complete(fetch_instance_ids(url, params))  
+    retracted_iids = loop.run_until_complete(fetch_instance_ids(url, params))
 
     # Get all the latest entries
     df_all = bq.get_latest()
 
     # Find all entries that match ESGF retractions
-    df_retracted = df_all[df_all['instance_id'].isin(retracted_iids)]
+    df_retracted = df_all[df_all["instance_id"].isin(retracted_iids)]
 
     # Find all the entries that are not marked as retracted yet
-    to_retract = df_retracted[df_retracted['retracted'].isin([False])]
+    to_retract = df_retracted[df_retracted["retracted"].isin([False])]
 
     # Print statistics and create report html
+    to_retract.to_html("retraction_report.html")
+    # upload the report to the bucket
+    fs = gcsfs.GCSFileSystem()
+    report_path = f"cmip6/cmip6-pgf-ingestion-test/reports/retraction_report_{datetime.datetime.utcnow().isoformat()}.html"
+    fs.put("retraction_report.html", f"gs://{report_path}")
+
     print(
         f"Got {len(retracted_iids)} retractions from ESGF.\n"
         f"{len(df_retracted)} of our stores are affected.\n"
         f"{len(to_retract)} stores will be newly marked as retracted.\n"
-        f"See retraction_report.html for details"
+        f"See http://storage.googleapis.com{report_path} for details"
     )
-    to_retract.to_html('retraction_report.html')
 
     ## Create IIDEntry objects for all the entries that need to be retracted
     iid_entries_to_retract = []
-    for idx, row in to_retract.iloc[:3,:].iterrows(): #TODO: remove the [:3,:] for production
-        iid_entry = IIDEntry(iid=row.instance_id, store=row.store, retracted=True, tests_passed=row.tests_passed)
+    for idx, row in to_retract.iloc[
+        :3, :
+    ].iterrows():  # TODO: remove the [:3,:] for production
+        iid_entry = IIDEntry(
+            iid=row.instance_id,
+            store=row.store,
+            retracted=True,
+            tests_passed=row.tests_passed,
+        )
         iid_entries_to_retract.append(iid_entry)
 
     # set values to retract in batches
-    batchsize = 1000 # not sure what the max no of entries is that bq can handle at once
-    for batch_iid_entries in [iid_entries_to_retract[i:i+batchsize] for i in range(0,len(iid_entries_to_retract), batchsize)]:
+    batchsize = (
+        1000  # not sure what the max no of entries is that bq can handle at once
+    )
+    for batch_iid_entries in [
+        iid_entries_to_retract[i : i + batchsize]
+        for i in range(0, len(iid_entries_to_retract), batchsize)
+    ]:
         bq.insert_multiple_iids(batch_iid_entries)
