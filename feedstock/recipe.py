77a1e0e4364878981db578e20b91c3d926bf1d2c
Julius Busecke <julius@ldeo.columbia.edu>
2024-02-10 12:25:17 -0500

    Test fix for multidim rechunking _invert_meshgrid error (#95)
    * Test fix for multidim rechunking _invert_meshgrid error
    
    @cisaacstern and I just came up with a fix for https://github.com/pangeo-forge/pangeo-forge-recipes/pull/658 and I want to see if this alleviates some of the failures I have seen.
    
    * Update iids_pr.yaml
    
    * Try out MPI models that failed before.
    
    * Update iids_pr.yaml
    
    * Update iids.yaml
    
    * Update recipe.py

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -235,7 +235,7 @@ def dynamic_chunking_func(ds: xr.Dataset) -> Dict[str, int]:
     
     target_chunk_size='150MB'
     target_chunks_aspect_ratio = {
-        'time':20,
+        'time':10,
         'x':1, 'i':1, 'ni':1, 'xh':1, 'nlon':1, 'lon':1, # TODO: Maybe import all the known spatial dimensions from xmip?
         'y':1, 'j':1, 'nj':1, 'yh':1, 'nlat':1, 'lat':1,
     }
