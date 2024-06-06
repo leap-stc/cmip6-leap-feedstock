1f0825993b9457cf83fe83d9351e8b66d14c8afb
Julius Busecke <julius@ldeo.columbia.edu>
2024-06-06 18:01:08 -0400

    Update recipe.py

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -162,11 +162,11 @@ for iid, data in recipe_data.items():
         f"Creating {iid}" >> beam.Create(pattern.items())
         | CheckpointFileTransfer(
             transfer_target=cache_target,
-            max_executors=10,
-            concurrency_per_executor=3,
+            max_executors=2,
+            concurrency_per_executor=2,
             initial_backoff=3.0,  # Try with super long backoff and
             backoff_factor=2.0,
-            fsspec_sync_patch=False,
+            fsspec_sync_patch=True,
         )
         | OpenURLWithFSSpec(cache=None, fsspec_sync_patch=True)
         | OpenWithXarray(xarray_open_kwargs={"use_cftime": True})
