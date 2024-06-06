e56963ca51673d9dd9af8677f73158247f0ff0aa
Julius Busecke <julius@ldeo.columbia.edu>
2024-06-06 10:07:48 -0400

    use fsspec

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -162,11 +162,11 @@ for iid, data in recipe_data.items():
         f"Creating {iid}" >> beam.Create(pattern.items())
         | CheckpointFileTransfer(
             transfer_target=cache_target,
-            max_executors=1,
-            concurrency_per_executor=2,
+            max_executors=10,
+            concurrency_per_executor=3,
             initial_backoff=3.0,  # Try with super long backoff and
             backoff_factor=2.0,
-            fsspec_sync_patch=True,
+            fsspec_sync_patch=False,
         )
         | OpenURLWithFSSpec(cache=None, fsspec_sync_patch=True)
         | OpenWithXarray(xarray_open_kwargs={"use_cftime": True})
